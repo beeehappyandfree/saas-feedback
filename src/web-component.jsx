6dfd897250e5d86a057832bd52533c97c21af0c6
beeehappyandfree <beeehappyandfree@gmail.com>
2024-10-04 16:55:31 +0800

    WIP Widget

--- src/web-component.jsx
+++ src/web-component.jsx
@@ -1,30 +1,29 @@
 import ReactDom from "react-dom/client";
 import { Widget } from "./components/Widget";
 
-export const normalizeAttribute = (attr) => {
-    return attr.replace( /-[a-z]/g,
-    (_,letter)=> toUpperCase())
-}
+export const normalizeAttribute = (attribute) => {
+  return attribute.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+};
 
 class WidgetWebComponent extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: "open"})
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    connectedCallback() {
-        const props = this.getPropsFromAttributes();
-        const root = ReactDom.createRoot(this.shadowRoot);
-        root.render(< Widget {...props}/>);
-    }
+  connectedCallback() {
+    const props = this.getPropsFromAttributes();
+    const root = ReactDom.createRoot(this.shadowRoot);
+    root.render(<Widget {...props} />);
+  }
 
-    getPropsFromAttributes() {
-        const props = {};
-        for (const {name, value} of this.attributes) {
-            props[normalizeAttribute(name)] = value;
-        }
-        return props;
+  getPropsFromAttributes() {
+    const props = {};
+    for (const { name, value } of this.attributes) {
+      props[normalizeAttribute(name)] = value;
     }
+    return props;
+  }
 }
 
 export default WidgetWebComponent;
